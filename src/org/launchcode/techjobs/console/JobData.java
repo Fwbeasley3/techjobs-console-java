1b9ff83acbd252f7e12428d7c93f45fcdcdc512e
Freddie Beasley <fwbeasley3@yahoo.com>
2019-05-23 18:35:47 -0500

    final commit

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -76,6 +77,9 @@ public class JobData {
 
             String aValue = row.get(column);
 
+            aValue = aValue.toLowerCase();
+            value = value.toLowerCase();
+
             if (aValue.contains(value)) {
                 jobs.add(row);
             }
@@ -83,6 +87,29 @@ public class JobData {
 
         return jobs;
     }
+
+    public static ArrayList<HashMap<String, String>>findByValue(String value){
+        loadData();
+
+        ArrayList<HashMap<String,String>> jobs = new ArrayList<>();
+
+
+        for (HashMap<String, String> row : allJobs) {
+            for (String key : row.keySet()) {
+                if (row.get(key).toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(row);
+                    break;
+                        }
+                    }
+
+                }
+
+        return jobs;
+            }
+
+
+
+
 
     /**
      * Read in data from a CSV file and store it in a list
